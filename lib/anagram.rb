75dd950604a04cf974bf793e700e79129956b36b
jerry <brownjer3@gmail.com>
2020-10-27 13:24:23 -0700

    commit tree fdd7031a668a55af3c72dd127dc5a23ce82c136b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select do |arr_word|
+            if arr_word.split("").sort == @word.split("").sort
+                arr_word
+            end
+        end
+    end
+
+end
